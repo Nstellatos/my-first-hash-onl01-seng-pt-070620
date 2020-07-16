9a53904391329f231f8b1205f7fbf9034d741b20
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-16 19:04:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,6 +28,9 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+                 shipping_manifest["muskets"]=3
+                 shipping_manifest["gun powder"]=4
+                 shipping_manifest
   # add 2 muskets to the shipping_manifest hash below
 
 
